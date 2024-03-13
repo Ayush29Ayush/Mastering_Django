6a83b8f84c652561639341c1eec8e50d64a83aa9
Ayush Senapati <ayush.senapati@cozentus.com>
2024-03-13 16:16:19 +0530

    Added view and their respective urls

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name="index"),
+    # path('index/', views.index, name="index"),
+    path('index/', views.Index.as_view(), name="index"),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def index(request):
-    # return HttpResponse("<h1>Hello, Ayush...</h1>")
-    return render(request, 'firstapp/index.html')
+# def index(request):
+#     age = 20
+#     arr = ["ayush", "hey", "world", "dragon"]
+#     dic = {"a": "one", "b": "two"}
+
+#     context = {
+#         "age": age,
+#         "array": arr,
+#         "dic": dic
+
+#     }
+#     return render(
+#         request,
+#         "firstapp/index.html",
+#         context=context
+#     )
+#     # return HttpResponse("<h1>Hello</h1>")
+
+
+class Index(TemplateView):
+    template_name = "firstapp/index.html"
+
+    def get_context_data(self, **kwargs):
+        age = 10
+        arr = ["ayush", "hey", "world", "dragon"]
+        dic = {"a": "one", "b": "two"}
+        context_old = super().get_context_data(**kwargs)
+        context = {"age": age, "array": arr, "dic": dic, "context_old": context_old}
+        return context
